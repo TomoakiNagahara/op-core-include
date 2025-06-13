303f77d2a75dc1652522c66e72284178457124ac
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-13 18:04:54 +0900

    New: Error.php

---
+++ Error.php
@@ -0,0 +1,12 @@
+<?php
+/**	op-core-include:/Error.php
+ *
+ * @genesis    ????-??-??  generated from op-core-4
+ * @created    2014-02-18  Separated from op-core-5:/App.class.php
+ * @porting    2016-12-07  Porting to op-core-7:/Error.php
+ * @moved      2025-06-11  Moved from op-core-7:/Error.php
+ * @version    1.0
+ * @package    op-core-include
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
