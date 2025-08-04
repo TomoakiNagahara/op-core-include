3eeb0a8a6f0f8dc5d822bd1b752ca22f61f4b9e1
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-08-04 21:41:20 +0900

    Fix: RequestWeb.php | No longer mixes GET and POST.

--- RequestWeb.php
+++ RequestWeb.php
@@ -16,7 +16,12 @@ if(($_SERVER['CONTENT_TYPE'] ?? null) === 'application/json' ){
 	//	Including this will not change the amount of memory consumed.
 	$_content = file_get_contents("php://input");
 	$_request = json_decode($_content, true);
+}else{
+	//	...
+	foreach( ($_SERVER['REQUEST_METHOD'] === 'POST' ? $_POST : $_GET) as $_key => $_val ){
+		$_request[$_key] = $_val;
+	}
 }
 
-//	Web
-$_request = array_merge_recursive($_request, $_GET ?? [], $_POST ?? []);
+//	...
+unset($_key, $_val);
